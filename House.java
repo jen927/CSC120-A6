13f085d9aa2d7612da8ae0a3bd5e4752d6fdacd4
jen927 <157835344+jen927@users.noreply.github.com>
2024-03-07 16:40:04 -0500

    added constructor and methods to library and house

--- House.java
+++ House.java
@@ -1,12 +1,47 @@
 /* This is a stub for the House class */
-public class House {
 
-  public House() {
-    System.out.println("You have built a house: 🏠");
+import java.util.ArrayList;
+
+public class House extends Building{
+
+  private ArrayList<String> residents; // The <String> tells Java what kind of data we plan to store IN the ArrayList
+  private boolean hasDiningRoom;
+
+  public House(String name, String address, int nFloors, boolean hasDiningRoom) {
+    super(name, address, nFloors); // is a building
+    //with...
+    this.hasDiningRoom = hasDiningRoom;
+    this.residents = new ArrayList<String>();
+    
   }
+  public boolean hasDiningRoom(){
+    return hasDiningRoom;
+  }
+  public int nResidents(){
+    return residents.size();
+  }
+  public void moveIn(String name){
+    residents.add(name);
+  }
+  public String moveOut(String name){ // return the name of the person who moved out
+    residents.remove(name);
+    return name;
+  }
+  public boolean isResident(String person){
+    return residents.contains(name);
+  }
+  public String toString() {
+    String desc = super.toString();
+    desc = "RESIDENCE HALL: " + desc;
+    if (hasDiningRoom == true) {
+      desc += ". It has a dining room. ";
+    }
+    return desc;
+}
 
   public static void main(String[] args) {
-    new House();
+    House myHouse = new House("Morrow", "3 Elm St", 4, false);
+    System.out.println(myHouse);
   }
 
 }
--- Library.java
+++ Library.java
@@ -1,12 +1,67 @@
 /* This is a stub for the Library class */
-public class Library {
 
-    public Library() {
-      System.out.println("You have built a library: 📖");
-    }
+import java.util.Hashtable;
+
+public class Library extends Building{
+
+  private Hashtable<String, Boolean> collection;
   
-    public static void main(String[] args) {
-      new Library();
-    }
   
+  public Library(String name, String address, int nFloors) {
+    super(name, address, nFloors); //is a building
+    //with...
+    this.collection = new Hashtable<String, Boolean>();
+    System.out.println("You have built a library: 📖");
   }
+  
+  public void addTitle(String title){
+    collection.put(title, true);
+  }
+  public String removeTitle(String title){ // return the title that we removed
+    collection.remove(title);
+    return title + " is removed from library.";
+  }
+  
+  public void checkOut(String title){
+    collection.replace(title, true, false);
+  }
+  public void returnBook(String title){
+    collection.replace(title, false, true);
+  }
+  public boolean containsTitle(String title){ // returns true if the title appears as a key in the Libary's collection, false otherwise
+    return collection.containsKey(title);
+  }
+  public boolean isAvailable(String title){ // returns true if the title is currently available, false otherwise
+    return collection.get(title).equals(true);
+  }
+  //public void printCollection(){ // prints out the entire collection in an easy-to-read way (including checkout status)
+   // System.out.println("List of books in collection:");
+   // collection.forEach((key, value)
+   // -> System.out.println("Title: " + key + "\t\t Availability: " + value));
+  //}
+  public void printCollection(){ // prints out the entire collection in an easy-to-read way (including checkout status)
+    System.out.println("List of books in collection:");
+    collection.forEach((key, value) -> {
+      if (isAvailable(key)){
+        String status = "In";
+        System.out.println("Title: " + key + "\t Availability: " + status);
+      }
+      else{
+        String status = "Out";
+        System.out.println("Title: " + key + "\t Availability: " + status);
+      }
+    });
+    
+    
+  } 
+  public static void main(String[] args) {
+   Library mylibrary = new Library("Neilson Libarary", "123 W ST", 4);
+   mylibrary.addTitle("Clear and Muddy Loss of Love");
+   mylibrary.addTitle("Grandmaster of Demonic Cultivation");
+   mylibrary.checkOut("Clear and Muddy Loss of Love");
+   //System.out.println(mylibrary.isAvailable("Clear and Muddy Loss of Love"));
+   //System.out.println(mylibrary.collection.get("Clear and Muddy Loss of Love"));
+   mylibrary.printCollection();
+  }
+  
+}
